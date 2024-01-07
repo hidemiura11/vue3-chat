030bed0d72293715bde108e6eb0e8afe2ecc8155
hidems <miura.eleven@gmail.com>
2024-01-07 21:53:27 +0100

    Remove unnecesary files and codes

---
+++ src/components/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>test</div>
+</template>
+
+<script>
+export default defineComponent({
+})
+</script>
+
+
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './components/App.vue'
 import store from './store'
 
 createApp(App).use(store).mount('#app')
