594935118785924a5680445f363c0fdd6a935cb2
hidems <miura.eleven@gmail.com>
2024-01-09 22:28:54 +0100

    Copy stores

--- src/main.js
+++ src/main.js
@@ -3,5 +3,14 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './components/App.vue'
 import store from './store'
+// import { getAllMessages } from './store/actions'
 
-createApp(App).use(store).mount('#app')
+// createApp(App).use(store).mount('#app')
+
+const app = createApp(App)
+
+app.use(store)
+
+app.mount('#app')
+
+// getAllMessages(store)
